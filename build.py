da310e6220739db3764d82ca3abaadf0737a98d6
MojaveWastelander <nsdapps@live.com>
2016-09-21 22:25:17 +0300

    Some libraries requires C99, will use msvc 12 and above for now.

--- build.py
+++ build.py
@@ -3,4 +3,12 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(args='--build=missing', username="MojaveWastelander")
     builder.add_common_builds(pure_c=False)
+    filtered_builds = []
+    for settings, options in builder.builds:
+        if settings["compiler"] == "Visual Studio":
+            if float(str(settings["compiler.version"])) >= 12:
+                filtered_builds.append([settings, options])
+        else:
+            filtered_builds.append([settings, options])
+    builder.builds = filtered_builds
     builder.run()
