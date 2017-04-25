dc273daf2d5ea3ee8e131cf2d35876d6461bb823
西风逍遥游 <sunxfancy@gmail.com>
2017-04-25 08:27:20 +0800

    change username
    test on ci

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args='--build=missing', username="MojaveWastelander")
+    builder = ConanMultiPackager(args='--build=missing', username="sunxfancy")
     builder.add_common_builds(pure_c=False)
     filtered_builds = []
     for settings, options in builder.builds:
         if settings["compiler"] == "Visual Studio":
-            if float(str(settings["compiler.version"])) >= 14: 
+            if float(str(settings["compiler.version"])) >= 14:
                 filtered_builds.append([settings, options])
         else:
             filtered_builds.append([settings, options])
