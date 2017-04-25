33e000c7de4c80eb7dd31e8d14b2f599f3fdb766
西风逍遥游 <sunxfancy@gmail.com>
2017-04-25 09:11:19 +0800

    update

--- build.py
+++ build.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(args='--build=missing', username="sunxfancy")
     builder.add_common_builds(pure_c=False)
     filtered_builds = []
-    for settings, options in builder.builds:
+    for settings, options, env_vars, build_requires in builder.builds:
         if settings["compiler"] == "Visual Studio":
             if float(str(settings["compiler.version"])) >= 14:
-                filtered_builds.append([settings, options])
+                filtered_builds.append([settings, options, env_vars, build_requires])
         else:
             filtered_builds.append([settings, options])
     builder.builds = filtered_builds
