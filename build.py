bdd5f10cd42af93f7a0fa3d1d8afa7cb3f12427e
Norgerman <xyn0410@gmail.com>
2017-04-25 19:48:56 +0800

    fix again

--- build.py
+++ build.py
@@ -4,8 +4,16 @@ import os
 if __name__ == "__main__":
     builder = ConanMultiPackager(args='--build=missing', username="sunxfancy")
     builder.add_common_builds(pure_c=False)
-    if os.environ["APPVEYOR_BUILD_WORKER_IMAGE"] == "Visual Studio 2017":
-        builder.builds[0]["compiler.version"] = 15
-    else:
-        builder.builds[0]["compiler.version"] = 14
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["compiler"] == "Visual Studio":
+            if os.environ["APPVEYOR_BUILD_WORKER_IMAGE"] == "Visual Studio 2017":
+                settings["compiler.version"] = 15
+            else:
+                settings["compiler.version"] = 14
+            filtered_builds.append([settings, options, env_vars, build_requires])
+        else:
+            filtered_builds.append([settings, options])
+    
+    builder.builds = filtered_builds    
     builder.run()
