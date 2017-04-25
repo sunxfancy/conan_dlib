79a3d0fe7bba932727b40f05dd1070afb5ec7ad1
西风逍遥游 <sunxfancy@gmail.com>
2017-04-25 16:43:28 +0800

    build only vs14 and vs15

--- build.py
+++ build.py
@@ -1,14 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args='--build=missing', username="sunxfancy")
+    builder = ConanMultiPackager(args='--build=missing', username="sunxfancy", default_visual_versions=['14', '15'])
     builder.add_common_builds(pure_c=False)
-    filtered_builds = []
-    for settings, options, env_vars, build_requires in builder.builds:
-        if settings["compiler"] == "Visual Studio":
-            if float(str(settings["compiler.version"])) >= 14:
-                filtered_builds.append([settings, options, env_vars, build_requires])
-        else:
-            filtered_builds.append([settings, options])
-    builder.builds = filtered_builds
     builder.run()
