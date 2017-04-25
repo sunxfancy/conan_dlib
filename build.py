45e678674b91c961016bf863aa00e344801579a8
西风逍遥游 <sunxfancy@gmail.com>
2017-04-25 16:49:06 +0800

    fix

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(args='--build=missing', username="sunxfancy", default_visual_versions=['14', '15'])
+    builder = ConanMultiPackager(args='--build=missing', username="sunxfancy", visual_versions=['14', '15'])
     builder.add_common_builds(pure_c=False)
     builder.run()
